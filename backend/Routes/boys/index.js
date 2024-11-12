7163b20d70121867a2295ff0c9b1863d66637eac
MaheshNadendla <maheshnadendla147@gmail.com>
2024-11-12 16:19:36 +0530

    next

--- backend/Routes/boys/index.js
+++ backend/Routes/boys/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const route = express.Router();
 
 
-const { create,finduser,updateUser,del,getAll,findInTotalBoysAndFindInCollegeBoysAndSendBoysHome } = require("../../Controllers/boys/index.js");
+const { create,finduser,updateUser,del,getAll,findInTotalBoysAndFindInCollegeBoysAndSendBoysHome,TotalBoysInHome,findingABoyInHome ,findingABoyCollege} = require("../../Controllers/boys/index.js");
 route.post("/totalboys",create);
 route.get("/totalboys/:id",finduser);
 route.get("/totalboys/",getAll);
@@ -11,6 +11,10 @@ route.put("/totalboys/:id",updateUser);
 route.delete("/totalboys/:id",del);
 
 route.put("/boyshomes/:id",findInTotalBoysAndFindInCollegeBoysAndSendBoysHome);
+route.get("/boyshomes/",TotalBoysInHome);
+route.get("/boyshomes/:id",findingABoyInHome);
+route.get("/boysin/:id",findingABoyCollege);
+
 
 
 
